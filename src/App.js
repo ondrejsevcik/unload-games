3d967de60a65fd8d557839a7dd81aa56cd9575fc
Ondrej Sevcik <ondrej.sev@gmail.com>
2020-04-17 17:26:58 +0200

    Add main menu
    and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import GuessTheFlag from './guess-the-flag';
+import React from "react";
+import GuessTheFlag from "./guess-the-flag";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
-function App() {
-  return <GuessTheFlag />
+const routes = {
+  home: "/",
+  guessTheFlag: "/guess-the-flag"
+};
+
+export function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path={routes.guessTheFlag}>
+          <GuessTheFlag />
+        </Route>
+        <Route path={routes.home}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return (
+    <div className="tc">
+      <h1>Pick a game</h1>
+      <nav>
+        <Link to={routes.guessTheFlag} className="tc">
+          Guess the flag
+        </Link>
+      </nav>
+    </div>
+  );
+}
