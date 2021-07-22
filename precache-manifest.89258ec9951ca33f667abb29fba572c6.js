0fab92e74ed5b469eaab5f56e0c0eebb0e9fcc8b
ondrejsevcik <hi@ondrejsevcik.com>
2021-07-22 09:12:53 +0000

    Deploying to gh-pages from @ ondrejsevcik/unload-games@15cf65de9e24142b532241b1740485e347cb2ee9 🚀

---
+++ precache-manifest.89258ec9951ca33f667abb29fba572c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610d276dd1473d2089182eea5a410374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f78f6534a02db44219cb",
+    "url": "/static/css/main.91a1163a.chunk.css"
+  },
+  {
+    "revision": "9b13fa116fc32e036d88",
+    "url": "/static/js/2.692cb5d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.692cb5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78f6534a02db44219cb",
+    "url": "/static/js/main.4a77ba45.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  }
+]);
