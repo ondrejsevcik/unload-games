d89d015df9d7a30e99a0ceda951349d7ccfbc449
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-04-22 17:47:01 +0200

    Switch to HashRouter
    To make it work in GH pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import GuessTheFlag from "./guess-the-flag";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./home-page";
 import { appRoutes } from "./app-routes";
 
 export function App() {
   return (
     <div className="max-w-sm m-auto">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path={appRoutes.guessTheFlag} element={<GuessTheFlag />} />
           <Route path={appRoutes.home} element={<HomePage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
