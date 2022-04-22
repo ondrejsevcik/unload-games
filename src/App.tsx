9e07311919decb22c40d04cab8902549ab83df54
Ondrej Sevcik <hi@ondrejsevcik.com>
2022-04-22 16:03:41 +0200

    Update react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 import GuessTheFlag from "./guess-the-flag";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./home-page";
 import { appRoutes } from "./app-routes";
 
 export function App() {
   return (
     <div className="max-w-sm m-auto">
-      <Router>
-        <Switch>
-          <Route path={appRoutes.guessTheFlag}>
-            <GuessTheFlag />
-          </Route>
-          <Route path={appRoutes.home}>
-            <HomePage />
-          </Route>
-        </Switch>
-      </Router>
+      <BrowserRouter>
+        <Routes>
+          <Route path={appRoutes.guessTheFlag} element={<GuessTheFlag />} />
+          <Route path={appRoutes.home} element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
